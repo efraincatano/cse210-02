37958de7c7867c0a39d2cbd9d63dcd32c2050a6e
andy_javab99@hotmail.es <andy_javab99@hotmail.es>
2022-01-25 17:49:14 -0500

    matias part

---
+++ guessing.py
@@ -0,0 +1,17 @@
+import random
+
+def guessing():
+    
+    choice = ""
+
+    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    choice = input('Would you like to continue playing? (y/n) ')
+
+    if choice == 'y':
+        card = cards[random.randint(0, 12)]
+        print(f'Current Card: {card}')
+    elif choice == 'n':
+        print('Thanks for playing. End of game.')
+    elif choice != 'y' and choice != 'n':
+        print('Error, your choice is not available')
