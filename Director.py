1d21f4d0a6534d91877a0aba990ba11ae9c80fb9
Efrain Cataño <efrain.catano97@gmail.com>
2022-01-26 20:09:19 -0300

    Changes card and director -Efrain

---
+++ Director.py
@@ -0,0 +1,6 @@
+import random
+from card import Card
+
+tarjet = Card()
+display = tarjet.displaying_card()
+print(display)
--- card.py
+++ card.py
@@ -3,14 +3,12 @@ import random
 class Card:
 
     def __init__(self):
-        pass
-        #self.card = [random.randint(0, 12)]
-        #self.displaying_card
+        
+        self.card = [random.randint(0, 12)]
+        self.displaying_card
 
     def displaying_card(self):
         self.card = [random.randint(0, 12)]
         return (self.card)
 
-if __name__ == '__main__':
-    tarjeta = Card
-    print(tarjeta.displaying_card)
+
