51a56b114cabd1a5498492d00d4c8068d3450c85
Efrain Cataño <efrain.catano97@gmail.com>
2022-01-26 19:58:00 -0300

    adding Card class methods -Efrain

--- card.py
+++ card.py
@@ -1,3 +1,16 @@
 import random
+
 class Card:
-    pass
+
+    def __init__(self):
+        pass
+        #self.card = [random.randint(0, 12)]
+        #self.displaying_card
+
+    def displaying_card(self):
+        self.card = [random.randint(0, 12)]
+        return (self.card)
+
+if __name__ == '__main__':
+    tarjeta = Card
+    print(tarjeta.displaying_card)
